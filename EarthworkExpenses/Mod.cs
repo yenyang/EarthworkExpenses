864f05accb1631ed46412a542b8972d25c9edd81
yenyang <tjkelley19@gmail.com>
2026-01-02 08:18:01 -0800

    Add project files.

---
+++ EarthworkExpenses/Mod.cs
@@ -0,0 +1,28 @@
+﻿using Colossal.Logging;
+using EarthworkExpenses.Systems;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace EarthworkExpenses
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(EarthworkExpenses)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            updateSystem.UpdateAt<TempBrushSystem>(SystemUpdatePhase.Modification4);
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
---
+++ EarthworkExpenses/Systems/TempBrushSystem.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="ModifyNetCompositionDataSystem.cs" company="Yenyang's Mods. MIT License">
+// Copyright (c) Yenyang's Mods. MIT License. All rights reserved.
+// </copyright>
+
+using Colossal.Entities;
+using Game;
+using Game.Common;
+using System;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace	EarthworkExpenses.Systems
+{
+    public partial class TempBrushSystem : GameSystemBase
+    {
+        private EntityQuery m_TempBrushQuery;
+        private ModificationBarrier4 m_Barrier;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            // Get System instances for other systems. 
+            m_Barrier = World.GetOrCreateSystemManaged<ModificationBarrier4>();
+
+            // Establish query for relevant entity components.
+            m_TempBrushQuery = SystemAPI.QueryBuilder()
+                .WithAllRW<Game.Tools.Temp>()
+                .WithAll<Game.Tools.Brush>()
+                .WithNone<Game.Common.Deleted>()
+                .Build();
+
+            RequireForUpdate(m_TempBrushQuery);
+
+
+            Mod.log.Info($"{nameof(TempBrushSystem)}.{nameof(OnCreate)}");
+        }
+
+        protected override void OnUpdate()
+        {
+            NativeArray<Entity> entities = m_TempBrushQuery.ToEntityArray(Allocator.Temp);
+            EntityCommandBuffer buffer = m_Barrier.CreateCommandBuffer();
+
+            for (int i = 0; i < entities.Length; i++)
+            {
+                if (entities[i] == Entity.Null ||
+                   !EntityManager.TryGetComponent(entities[i], out Game.Tools.Temp temp))
+                {
+                    continue;
+                }
+
+                temp.m_Cost = 100;
+                buffer.SetComponent(entities[i], temp);
+            }
+            
+        }
+    }
+}
+
